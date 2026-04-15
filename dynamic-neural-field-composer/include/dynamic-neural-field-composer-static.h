d5df273634667cfaf66f5ac04574b9464cc69e0b
Jgocunha <gasparcunha19@outlook.pt>
2026-04-15 09:06:37 +0100

    Add static header for dynamic neural field composer dependencies

---
+++ dynamic-neural-field-composer/include/dynamic-neural-field-composer-static.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#include "visualization/visualization.h"
+#include "application/application.h"
+#include "user_interface/static_layout_window.h"
+#include "user_interface/main_menu_bar.h"
