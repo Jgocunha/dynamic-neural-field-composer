aaef53fd5795ccfde51b7600ef07253005deab6b
Jgocunha <Jgocunha@users.noreply.github.com>
2026-08-05 11:45:12 +0000

    deploy: efbd32b1e3ef903eca0f9a296b6c2ebe894d7625

--- search/classes_3.js
+++ search/classes_3.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['element_0',['Element',['../classdnf__composer_1_1element_1_1Element.html',1,'dnf_composer::element']]],
-  ['elementcategoryinfo_1',['ElementCategoryInfo',['../structdnf__composer_1_1element_1_1ElementCategoryInfo.html',1,'dnf_composer::element']]],
-  ['elementcommonparameters_2',['ElementCommonParameters',['../structdnf__composer_1_1element_1_1ElementCommonParameters.html',1,'dnf_composer::element']]],
-  ['elementdimensions_3',['ElementDimensions',['../structdnf__composer_1_1element_1_1ElementDimensions.html',1,'dnf_composer::element']]],
-  ['elementfactory_4',['ElementFactory',['../classdnf__composer_1_1element_1_1ElementFactory.html',1,'dnf_composer::element']]],
-  ['elementidentifiers_5',['ElementIdentifiers',['../structdnf__composer_1_1element_1_1ElementIdentifiers.html',1,'dnf_composer::element']]],
-  ['elementspecificparameters_6',['ElementSpecificParameters',['../structdnf__composer_1_1element_1_1ElementSpecificParameters.html',1,'dnf_composer::element']]],
-  ['elementwindow_7',['ElementWindow',['../classdnf__composer_1_1user__interface_1_1ElementWindow.html',1,'dnf_composer::user_interface']]],
-  ['exception_8',['Exception',['../classdnf__composer_1_1Exception.html',1,'dnf_composer']]],
-  ['expand_9',['Expand',['../classdnf__composer_1_1element_1_1Expand.html',1,'dnf_composer::element']]],
-  ['expandparameters_10',['ExpandParameters',['../structdnf__composer_1_1element_1_1ExpandParameters.html',1,'dnf_composer::element']]]
+  ['editorcontextdeleter_0',['EditorContextDeleter',['../structdnf__composer_1_1user__interface_1_1EditorContextDeleter.html',1,'dnf_composer::user_interface']]],
+  ['element_1',['Element',['../classdnf__composer_1_1element_1_1Element.html',1,'dnf_composer::element']]],
+  ['elementcategoryinfo_2',['ElementCategoryInfo',['../structdnf__composer_1_1element_1_1ElementCategoryInfo.html',1,'dnf_composer::element']]],
+  ['elementcommonparameters_3',['ElementCommonParameters',['../structdnf__composer_1_1element_1_1ElementCommonParameters.html',1,'dnf_composer::element']]],
+  ['elementdimensions_4',['ElementDimensions',['../structdnf__composer_1_1element_1_1ElementDimensions.html',1,'dnf_composer::element']]],
+  ['elementfactory_5',['ElementFactory',['../classdnf__composer_1_1element_1_1ElementFactory.html',1,'dnf_composer::element']]],
+  ['elementidentifiers_6',['ElementIdentifiers',['../structdnf__composer_1_1element_1_1ElementIdentifiers.html',1,'dnf_composer::element']]],
+  ['elementspecificparameters_7',['ElementSpecificParameters',['../structdnf__composer_1_1element_1_1ElementSpecificParameters.html',1,'dnf_composer::element']]],
+  ['elementwindow_8',['ElementWindow',['../classdnf__composer_1_1user__interface_1_1ElementWindow.html',1,'dnf_composer::user_interface']]],
+  ['exception_9',['Exception',['../classdnf__composer_1_1Exception.html',1,'dnf_composer']]],
+  ['expand_10',['Expand',['../classdnf__composer_1_1element_1_1Expand.html',1,'dnf_composer::element']]],
+  ['expandparameters_11',['ExpandParameters',['../structdnf__composer_1_1element_1_1ExpandParameters.html',1,'dnf_composer::element']]]
 ];
