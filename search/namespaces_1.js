4b7465a9be701bd4bd21a623af750a485cfab98a
Jgocunha <Jgocunha@users.noreply.github.com>
2026-08-17 20:04:24 +0000

    deploy: 27fd207a64506d4dd531309fa8be32c4088bb897

--- search/namespaces_1.js
+++ search/namespaces_1.js
@@ -9,5 +9,6 @@ var searchData=
   ['dnf_5fcomposer_3a_3atools_3a_3aprofiling_6',['profiling',['../namespacednf__composer_1_1tools_1_1profiling.html',1,'dnf_composer::tools']]],
   ['dnf_5fcomposer_3a_3atools_3a_3autils_7',['utils',['../namespacednf__composer_1_1tools_1_1utils.html',1,'dnf_composer::tools']]],
   ['dnf_5fcomposer_3a_3auser_5finterface_8',['user_interface',['../namespacednf__composer_1_1user__interface.html',1,'dnf_composer']]],
-  ['dnf_5fcomposer_3a_3auser_5finterface_3a_3awidgets_9',['widgets',['../namespacednf__composer_1_1user__interface_1_1widgets.html',1,'dnf_composer::user_interface']]]
+  ['dnf_5fcomposer_3a_3auser_5finterface_3a_3acolour_9',['colour',['../namespacednf__composer_1_1user__interface_1_1colour.html',1,'dnf_composer::user_interface']]],
+  ['dnf_5fcomposer_3a_3auser_5finterface_3a_3awidgets_10',['widgets',['../namespacednf__composer_1_1user__interface_1_1widgets.html',1,'dnf_composer::user_interface']]]
 ];
