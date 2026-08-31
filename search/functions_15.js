3c15b5055ecfe109e1706c7831222a1f0547f1dc
Jgocunha <Jgocunha@users.noreply.github.com>
2026-08-31 13:02:41 +0000

    deploy: bbb8adf5b759200e47098c26b18eb315964be6c6

--- search/functions_15.js
+++ search/functions_15.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['what_0',['what',['../classdnf__composer_1_1Exception.html#a52961c9cc2656b73d7767f5a3840a4b5',1,'dnf_composer::Exception']]],
-  ['wrap_1',['wrap',['../namespacednf__composer_1_1tools_1_1math.html#a84a4dfc370130a2e03c6d9cfb7939f26',1,'dnf_composer::tools::math']]],
-  ['writeweights_2',['writeWeights',['../classdnf__composer_1_1element_1_1FieldCoupling.html#ab74a05063b97a31c7f74ed6fd38b3954',1,'dnf_composer::element::FieldCoupling']]]
+  ['willfileloadsuccessfully_1',['willFileLoadSuccessfully',['../classdnf__composer_1_1SimulationFileManager.html#af63dee9c89401102c79cd153c6a6b78d',1,'dnf_composer::SimulationFileManager']]],
+  ['wrap_2',['wrap',['../namespacednf__composer_1_1tools_1_1math.html#a84a4dfc370130a2e03c6d9cfb7939f26',1,'dnf_composer::tools::math']]],
+  ['writeweights_3',['writeWeights',['../classdnf__composer_1_1element_1_1FieldCoupling.html#ab74a05063b97a31c7f74ed6fd38b3954',1,'dnf_composer::element::FieldCoupling']]]
 ];
